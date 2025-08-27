ee958ed81f83cccda7dce1fd5268828014ffef4f
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-08-27 06:22:28 +0330

    update

--- server/first/notes.js
+++ server/first/notes.js
@@ -4,3 +4,14 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 const { say } = require("cowsay");
+
+
+
+
+//  با این کد میتونیم خیلی سریع تر از find کلید مورد نمظرمون رو پیدا کنیم
+const categoriesMap = new Map(
+  categories.map(c => [c.category, c])
+);
+
+
+console.log(categoriesMap.get("drink")); 
