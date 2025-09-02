a256758d9cbcd4251c6d6b9b0c9bf9080484979c
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-09-02 10:12:48 +0330

    update

--- server/first/mongodb-homework/server/index.js
+++ server/first/mongodb-homework/server/index.js
@@ -14,27 +14,24 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "../../../.env") });
 const port = process.env.PORT || 6000;
 const mongoUri = process.env.MONGO_URI;
-const membersRouter= router;
+const membersRouter = router;
 
 server.use(cors());
-server.use(logger)
+server.use(logger);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 // main route:
-server.get("/", (req,res)=>{
-    res.json({message:"hellow this is the main route❤️"});
-})
+server.get("/", (req, res) => {
+  res.json({ message: "hellow this is the main route❤️" });
+});
 
 // members route:
-server.use("/api/members",membersRouter);
+server.use("/api/members", membersRouter);
 
-
-
-
-
-
-
+server.use((req, res, next) => {
+  res.status(404).json({ message: "404 page not found!❌" });
+});
 
 server.listen(port, async () => {
   try {
