8c245fdd4bccdfbf7ce972e436b6957ec23fe7ea
mohammad-niko <mohammadnikoo1900@gmail.com>
2025-09-20 11:44:00 +0330

    homework

---
+++ server/first/SQL 1/mysql1.js
@@ -0,0 +1,18 @@
+/* Task One
+1. Open MySQL Workbench and create a database named "office". Inside this database, create a table named "employee" with three columns: "id", "name", and "role".
+2. Once you've created the database and table, insert some fake employee information into the "employee" table. Write the relevant query to add these records.
+3. Update employee records in MySQL Workbench using SQL queries. For example, you can update the name of the employee with an id of 1.
+4. Delete employee records in MySQL Workbench using SQL queries. For example, you can delete the employee with an id of 3.
+5. Retrieve and view employee records in MySQL Workbench using SQL queries. For example, you can view the details of the employee with an id of 2.
+6. export your queries and send them to us.
+
+---------------------------------
+Task Two
+Open the "https://www.khanacademy.org/computing/computer-programming/sql" website and complete lessons 'Modifying databases with SQL' and 'SQL basics'. 
+To ensure you understand the material, we recommend reading or watching the lesson first.
+Then, complete the lesson tasks and take a screenshot of your completed work. 
+Please send the screenshot to us.
+
+GOOD LUCK
+BYE :D
+*/
