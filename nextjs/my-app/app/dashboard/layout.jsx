f79ecc59f9d5711dc76c7e83ab3a4e925a5a3e11
mohammad-niko <mohammadnikoo1900@gmail.com>
2026-02-09 11:45:25 +0330

    next-1

---
+++ nextjs/my-app/app/dashboard/layout.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Layout({ children }) {
+  
+  return (
+    <div>
+      <div>mmad niko Dashboard</div>
+      {children}
+    </div>
+  );
+}
---
+++ nextjs/my-app/app/dashboard/not-found.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>NotFound 404❌</div>
+  )
+}
+
+export default NotFound
