8271faa32401eb3d7acbb2492de8ea923a4dc584
mohammad-niko <mohammadnikoo1900@gmail.com>
2026-02-11 21:49:43 +0330

    update

--- nextjs/my-app/app/dashboard/not-found.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFound() {
-  return (
-    <div>NotFound 404❌</div>
-  )
-}
-
-export default NotFound
---
+++ nextjs/my-app/app/races/error.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+function racesError() {
+  return (
+    <div>races Error</div>
+  )
+}
+
+export default racesError
